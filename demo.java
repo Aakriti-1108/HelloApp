c31479fa8880f786e3b095c20c4dc0ba096b6bc8
Aakriti Pathak <aakritipathak2006@.com>
2026-03-18 13:19:58 +0530

    main

--- demo.java
+++ demo.java
@@ -1,5 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+        String result = String.join(", ", args);
+        System.out.println("Hello, " + result + "!");
     }
 }
