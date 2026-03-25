28eb18cd63dc8e6f8d6fbe7f5e8d94703bfe6d1c
Aakriti Pathak <aakritipathak2006@.com>
2026-03-25 12:54:08 +0530

    main

---
+++ UC6.java
@@ -0,0 +1,24 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        // If no arguments, print default message
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Build the names string
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        // Print final greeting
+        System.out.println("Hello, " + names + "!");
+    }
+}
